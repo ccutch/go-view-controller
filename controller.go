7174f94891c111faba1077083180eef0f82b0569
Connor McCutcheon <connormccutcheon95@gmail.com>
2016-05-01 07:29:41 -0700

    First version

---
+++ controller.go
@@ -0,0 +1,56 @@
+package controller
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Route struct {
+	Path    string
+	Method  string
+	Handler func()
+}
+
+type Controller struct {
+	Prefix string
+	Data   interface{}
+	Res    http.ResponseWriter
+	Req    *http.Request
+	Routes []Route
+}
+
+func (c *Controller) Render(name string) {
+	temp, err := template.ParseFiles(name)
+	if err != nil {
+		c.Raw("Internal Server Error" + err.Error())
+		return
+	}
+	temp.Execute(c.Res, c.Data)
+}
+
+func (c *Controller) Raw(data string) {
+	c.Write([]byte(data))
+}
+
+func (c *Controller) Write(data []byte) (int, error) {
+	return c.Res.Write(data)
+}
+
+func (c *Controller) toHandleFunc(pred func()) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		pred()
+	}
+}
+
+func (c *Controller) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c.Req = r
+	c.Res = w
+	m := mux.NewRouter()
+	for _, route := range c.Routes {
+		m.HandleFunc(route.Path, c.toHandleFunc(route.Handler)).
+			Methods(route.Method)
+	}
+	m.ServeHTTP(w, r)
+}
---
+++ spec/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ccutch/view-controller"
+)
+
+type Application struct {
+	*controller.Controller
+}
+
+func (this Application) Home() {
+	this.Render("spec/views/application.html")
+}
+
+func main() {
+	a := Application{new(controller.Controller)}
+
+	a.Routes = []controller.Route{
+		controller.Route{
+			Path:    "/",
+			Method:  "GET",
+			Handler: a.Home,
+		},
+	}
+
+	http.Handle("/", a)
+	fmt.Println("Server online")
+	http.ListenAndServe(":8080", nil)
+}
