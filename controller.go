5973e5e8831a40dd4909fa918752aef48baa9509
Connor McCutcheon <connormccutcheon95@gmail.com>
2016-05-03 00:05:40 -0700

    Push for night fixing rendering

--- controller.go
+++ controller.go
@@ -53,7 +53,9 @@ func (c *Controller) Render(name string) error {
 
 func (c *Controller) RenderError(view string, err error) error {
 	t := template.Must(template.ParseFiles("views/errors/" + view + ".html"))
-	return t.Execute(c, err)
+	return t.Execute(c, struct {
+		Error string
+	}{Error: err.Error()})
 }
 
 func (c *Controller) Raw(data string) {
@@ -66,20 +68,18 @@ func (c *Controller) Write(data []byte) (int, error) {
 
 func (c *Controller) toHandleFunc(pred func()) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		c.Req = r
+		c.Res = w
 		c.Params = mux.Vars(r)
 		c.Options = r.URL.Query()
 		pred()
 	}
 }
 
-func (c *Controller) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	c.Req = r
-	c.Res = w
-
-	m := mux.NewRouter()
+func (c *Controller) Mount(router *mux.Router) {
 	for _, route := range c.Routes {
-		m.HandleFunc(route.Path, c.toHandleFunc(route.Handler)).
+		// TODO: Add prefix field to controller for path prefix
+		router.HandleFunc(route.Path, c.toHandleFunc(route.Handler)).
 			Methods(route.Method)
 	}
-	m.ServeHTTP(w, r)
 }
